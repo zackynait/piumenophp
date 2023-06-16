8770820a5cce8adcc55bc1a151059e16a7787dc8
pesce-giuseppe <pesce@graphiservice.it>
2023-06-16 10:42:52 +0200

    fix header buy_voucher

--- pages/buy_voucher.php
+++ pages/buy_voucher.php
@@ -239,9 +239,9 @@ $mysubs=SubscriptionModel::get_my_voucher( $_SESSION['id'])->fetch_all(MYSQLI_AS
         <nav aria-label="breadcrumb">
           <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
             <li class="breadcrumb-item text-sm"><a class="opacity-5 text-dark" href="javascript:;">Pagina</a></li>
-            <li class="breadcrumb-item text-sm text-dark active" aria-current="page">Admin</li>
+            <li class="breadcrumb-item text-sm text-dark active" aria-current="page">Acquista voucher</li>
           </ol>
-          <h6 class="font-weight-bolder mb-0">Admin</h6>
+          <h6 class="font-weight-bolder mb-0">Acquista voucher</h6>
         </nav>
         <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
 
