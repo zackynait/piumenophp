7fc2ca831236909bfbbcd7fb137f68230dc5591a
pesce-giuseppe <pesce@graphiservice.it>
2023-06-16 10:40:48 +0200

    fix header buy_track

--- pages/buy_track.php
+++ pages/buy_track.php
@@ -226,7 +226,7 @@ $allcaps=TrattaModel::get_all_caps()->fetch_all(MYSQLI_ASSOC);
 									</div> <span class="nav-link-text ms-1">RTL</span> </a>
 							</li>
 							<li class="nav-item mt-3">
-								<h6 class="ps-4 ms-2 text-uppercase text-xs font-weight-bolder opacity-6">Admin page</h6> </li>
+								<h6 class="ps-4 ms-2 text-uppercase text-xs font-weight-bolder opacity-6">Acquista tratta</h6> </li>
 							<li class="nav-item">
 								<a class="nav-link  " href="../pages/profile.html">
 									<div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
@@ -294,9 +294,9 @@ $allcaps=TrattaModel::get_all_caps()->fetch_all(MYSQLI_ASSOC);
         <nav aria-label="breadcrumb">
           <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
             <li class="breadcrumb-item text-sm"><a class="opacity-5 text-dark" href="javascript:;">Pagina</a></li>
-            <li class="breadcrumb-item text-sm text-dark active" aria-current="page">Admin</li>
+            <li class="breadcrumb-item text-sm text-dark active" aria-current="page">Acquista tratta</li>
           </ol>
-          <h6 class="font-weight-bolder mb-0">Admin</h6>
+          <h6 class="font-weight-bolder mb-0">Acquista tratta</h6>
         </nav>
         <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
 
@@ -684,6 +684,11 @@ $('div.setup-panel div a.btn-primary').trigger('click');
       },
     });
   </script>
+  <!--   Core JS Files   -->
+  <script src="../assets/js/core/popper.min.js"></script>
+  <script src="../assets/js/core/bootstrap.min.js"></script>
+  <script src="../assets/js/plugins/perfect-scrollbar.min.js"></script>
+  <script src="../assets/js/plugins/smooth-scrollbar.min.js"></script>
   <script>
     var win = navigator.platform.indexOf('Win') > -1;
     if (win && document.querySelector('#sidenav-scrollbar')) {
